f28f99a486f49d5bf51f1ac6c5947bf451828411
Pedro Werkhaizer <42971669+pedrowerkhaizer@users.noreply.github.com>
2024-09-19 23:00:38 -0300

    Update FirebaseConfig.ts

--- utils/FirebaseConfig.ts
+++ utils/FirebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDP-Iz8PEinfP1bbu5Cnl_d56uvr8jxWRc',
-  authDomain: 'wordle-c6031.firebaseapp.com',
-  projectId: 'wordle-c6031',
-  storageBucket: 'wordle-c6031.appspot.com',
-  messagingSenderId: '191974523581',
-  appId: '1:191974523581:web:3b3a736d33d1ba17932dd4',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 const FIREBASE_APP = initializeApp(firebaseConfig);
